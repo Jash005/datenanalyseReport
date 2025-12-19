cc78ca9b942b59d851e491e2ec98b90f23c6147d
Jasmin Hauschild <mail@jasminh.de>
2025-12-19 09:38:59 +0100

    löschen einer falschen datei

--- SANDBOX-Projekt-Report.R
+++
@@ -1,8 +0,0 @@
-rm(list=ls())
-
-TrafficIncident <- read.table(
-  file ="Projektarbeit/datenanalyseReport/dataset_traffic_accidents.csv", sep = ",", header=T, stringsAsFactors = F
-)
-head(TrafficIncident, 3)
-dim(TrafficIncident)
-glimpse(TrafficIncident)
